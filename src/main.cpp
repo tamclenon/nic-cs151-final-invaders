10cbd7c615eed0e95e361b69ce26f8d31d3f4cb6
Toby Mclenon <tamclenon@nic.edu>
2021-04-18 16:30:55 -0700

    Initial project creation

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello world!" << std::endl;
+}
