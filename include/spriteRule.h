6361d5fbacd09ede92cb5ea8229b03b51875371f
Shaun Martin <swmartin@students.nic.edu>
2021-04-22 10:38:52 -0700

    Build Test

---
+++ include/spriteRule.h
@@ -0,0 +1,40 @@
+#ifndef SPRITERULE_H
+#define SPRITERULE_H
+
+#include <string>
+using std::string;
+
+struct spriteDemention
+{
+    int spriteX;
+    int spriteY;
+
+    spriteDemention(int x = 1, int y = 1)
+    {
+        x > 0 ? spriteX = x : spriteX = 1;
+        y > 0 ? spriteY = y : spriteY = 1;
+    }
+};
+
+string SPRITE_TYPES[] = {"player", "saucer", "enemy1", "enemy2", "enemy3"};
+spriteDemention SPRITE_IMAGES[][2] = {{spriteDemention(), spriteDemention()}, 
+                                      {spriteDemention(), spriteDemention()}, 
+                                      {spriteDemention(), spriteDemention()}, 
+                                      {spriteDemention(), spriteDemention()}, 
+                                      {spriteDemention(), spriteDemention()}};
+
+class Sprite
+{
+public:
+    Sprite();
+    ~Sprite();
+
+protected:
+
+private:
+    string type;
+    int health;
+    spriteDemention hitBox; 
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 #include "game.h"
+#include "spriteRule.h"
+#include <iostream>
+using namespace std;
 
 
 
 int main()
 {
-	Game game;
+	cout << SPRITE_IMAGES[0][0].spriteX << ' ' << SPRITE_IMAGES[0][0].spriteY << '\n';
+	// Game game;
 
-	while (!game.isFinished())
-	{
-		game.input();
-		game.update();
-		game.render();
-	}
+	// while (!game.isFinished())
+	// {
+	// 	game.input();
+	// 	game.update();
+	// 	game.render();
+	// }
 	
 	return 0;
 }
